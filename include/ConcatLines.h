887cd895758e87919b9866d983a0740194c6c1df
Ayub Mohamed <ayufmohammed@gmail.com>
2024-05-17 23:38:42 -0600

    Viewing something , test

--- include/ConcatLines.h
+++ include/ConcatLines.h
@@ -13,19 +13,4 @@
 class App ;
 void concatLines( App* app );    // Function to keylog user input to view lines written by user
 
-=======
-#include <iostream> 
-#include <conio.h>
-#include <stdio.h>
-#include <windows.h>
-#include <tlhelp32.h>
-#include <thread>
 
-#define ENTER 13                 //  ASCII representation of ENTER key used to delineate lines entered by user
-
-#pragma once 
-
-class App ;
-
-void concatLines( App* app );    // Function to keylog user input to view lines written by user
->>>>>>> 10b4dd7d7c7cfcdc80b8db6b17089b629367b1da
--- src/ConcatLines.cpp
+++ src/ConcatLines.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "concatLines.h"
 #include "State.h"
 #include "App.h" //Cyclic Dependencies
@@ -37,42 +36,3 @@ void concatLines( App* app ) //Background process
 
 
 //------------------------------------------------------------------------------- Line Concatenation Implementation --------------------------------------------------------------------------------
-=======
-#include "concatLines.h"
-#include "State.h"
-#include "App.h"
-
-
-
-//------------------------------------------------------------------------------- Line Concatenation Implementation --------------------------------------------------------------------------------
-
-
-
-void concatLines( App* app ) //Background process
-{
-  std::uint8_t           previousValue               { 0 } ;                     // Char BUFFER to view previous user Input
-  std::uint8_t           BUFFER            =      _getch() ;
-
-  while(!_kbhit() && (_kbhit && app->isOpen()) )                                //Loop until App closes !_kbhit() && _kbhit() Facilitates infinite loop (Dum asf imo stop tryna b different)
-  {
-    if(static_cast<int>(BUFFER) == ENTER  && previousValue != ENTER)            // Whitespace handling , Check if User input is 13 ( 'ENTER' ascii ) && PreviousValue is not 'Enter' 
-    {
-      ++ app->lines_written                                 ;  
-    }
-    std::cout << BUFFER << std::endl; 
-                         previousValue     =        BUFFER  ;                    //Remove Consecutive Enter Values for adding whitespace . 
-                         BUFFER            =       _getch() ;
-    std::this_thread::sleep_for(std::chrono::milliseconds(50)); // Slight delay to reduce CPU usage
-    if(!app->isOpen()) 
-    {
-      break;
-    }
-  }
-
-
-  std::cout << app->lines_written;
-}
-
-
-//------------------------------------------------------------------------------- Line Concatenation Implementation --------------------------------------------------------------------------------
->>>>>>> 10b4dd7d7c7cfcdc80b8db6b17089b629367b1da
